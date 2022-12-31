ce22ecde645d997a7c68ecd759a52bff881074de
thaddeus0605 <tdronski@gmail.com>
2022-12-31 10:51:29 -0600

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Thaddeus Dronski on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
